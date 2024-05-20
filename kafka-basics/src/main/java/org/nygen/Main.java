4eeb74400cc6ec52aeb67cd0ac5fd6e159bc8135
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-05-20 23:09:23 +0530

    Created demo kafka producer

--- kafka-basics/src/main/java/org/nygen/Main.java
+++
@@ -1,7 +0,0 @@
-package org.nygen;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ kafka-basics/src/main/java/org/nygen/kafka/ProducerDemo.java
@@ -0,0 +1,7 @@
+package org.nygen.kafka;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Producer Demo");
+    }
+}
