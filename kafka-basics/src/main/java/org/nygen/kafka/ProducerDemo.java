70a814b52975f4b5bac20b658cb77c9b9957579b
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-05-20 23:50:50 +0530

    Kafka producer created

--- kafka-basics/src/main/java/org/nygen/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/org/nygen/kafka/ProducerDemo.java
@@ -1,7 +1,12 @@
 package org.nygen.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class ProducerDemo {
 
@@ -10,7 +15,28 @@ public class ProducerDemo {
     public static void main(String[] args) {
         log.info("Producer Started");
 
+        //Create Producer Properties to connect to local kafka
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty("bootstrap.servers", "localhost:9092");
 
+        producerProperties.setProperty("key.serializer", StringSerializer.class.getName());
+        producerProperties.setProperty("value.serializer", StringSerializer.class.getName());
+
+
+        //Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProperties);
+
+        //Create a Producer Record
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        //Send Data
+        producer.send(producerRecord);
+
+        producer.flush();
+        producer.close();
+
+        log.info("Producer Completed");
 
     }
 }
